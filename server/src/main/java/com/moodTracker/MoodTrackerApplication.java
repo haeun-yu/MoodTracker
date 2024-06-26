6fbec820612bafee9af02727840af75e3eff04da
bluewhale507 <jec9663@gmail.com>
2024-06-27 00:36:52 +0900

    CHORE : modify springBoot default port

--- server/src/main/java/com/moodTracker/MoodTrackerApplication.java
+++ server/src/main/java/com/moodTracker/MoodTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.moodTracker.moodTracker;
+package com.moodTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
