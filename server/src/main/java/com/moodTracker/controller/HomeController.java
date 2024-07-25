bf0f6cfc1377af3c93a53e66597ba39b1ce037d5
Eunchan Ju <79463235+bluewhale507@users.noreply.github.com>
2024-07-26 06:16:28 +0900

    Create HomeController.java

---
+++ server/src/main/java/com/moodTracker/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.moodTracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class HomeController {
+	
+	//Spring API 경로는 /api로 시작
+	@RequestMapping(value = "/{path:[^\\\\.]*}")
+	public String viewMapping() {
+		return "forward:/index.html";
+	}
+}
