e49cda2e8d3d29e3a1003596f101aea5d07523a5
haeun-yu <youhe@teamgrit.kr>
2024-07-28 00:10:44 +0900

    feat: gemini test page 추가

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -47,6 +47,11 @@ const router = createRouter({
       path: '/my',
       name: 'my',
       component: () => import('@/views/MyView.vue')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('@/views/GeminiTestView.vue')
     }
   ]
 })
--- client/src/views/ReportView.vue
+++ client/src/views/ReportView.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="flex flex-col gap-[30px]">
-    <section>
-      <h1 class="text-4xl-bold mb-[10px]">월간 리포트</h1>
-      <p>
-        {{ year }}년 {{ month }}월달의 감정 변화를 분석했어요. 나의 감정 패턴을 이해하고 되새겨보는
-        시간을 가져보세요.
-      </p>
+    <section class="w-full flex items-end justify-between">
+      <div>
+        <h1 class="text-4xl-bold mb-[10px]">월간 리포트</h1>
+        <p>
+          {{ year }}년 {{ month }}월달의 감정 변화를 분석했어요. 나의 감정 패턴을 이해하고
+          되새겨보는 시간을 가져보세요.
+        </p>
+      </div>
+      <!-- <div>
+        <button class="btn-primary p-[10px]" @click="prevMonth">
+          <p><</p>
+        </button>
+
+      </div> -->
     </section>
 
     <section v-if="!hasReport" class="w-full h-full flex items-center justify-center">
@@ -137,23 +145,22 @@ const user = ref<User | null>({
 })
 
 onBeforeMount(async () => {
-  // const checkLoginResponse = await authAPI.checkLogin()
-  // if (!checkLoginResponse) {
-  //   addToast({
-  //     message: '로그인이 필요합니다.'
-  //   })
-  //   router.push('/login')
-  // }
-
-  // const getUserResponse = await authAPI.getInformation()
-  // if (!getUserResponse) {
-  //   addToast({
-  //     message: '사용자 정보를 불러오는데 실패했습니다.'
-  //   })
-  //   router.push('/login')
-  // }
-  // user.value = getUserResponse
-
+  // 버튼 추가
+  const checkLoginResponse = await authAPI.checkLogin()
+  if (!checkLoginResponse) {
+    addToast({
+      message: '로그인이 필요합니다.'
+    })
+    router.push('/login')
+  }
+  const getUserResponse = await authAPI.getInformation()
+  if (!getUserResponse) {
+    addToast({
+      message: '사용자 정보를 불러오는데 실패했습니다.'
+    })
+    router.push('/login')
+  }
+  user.value = getUserResponse
   getReport()
 })
 
